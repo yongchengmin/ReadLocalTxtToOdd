cc4c4d965aa6765e4dc3ed9d6cda8da37ba8ccb0
yongchengmin <minyongcheng@qq.com>
2018-01-18 09:28:42 +0800

    删除无用引入

--- src/com/socket/WriteSocket.java
+++ src/com/socket/WriteSocket.java
@@ -2,14 +2,11 @@ package com.socket;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
